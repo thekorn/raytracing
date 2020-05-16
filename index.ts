82cd602988a363c13616a90d3885b2e8f1b0b63b
Markus Korn <markus.korn@gmail.com>
2020-05-16 15:57:59 +0200

    * rotate image by 90 degree

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ const imgHeight = 256
 
 const img = new PPMImageFile('/tmp/boo.ppm', imgWidth, imgHeight)
 
-for (let y = 0; y < imgHeight; y++) {
-  for (let x = 0; x < imgWidth; x++) {
+for (let y = imgHeight - 1; y >= 0 ; --y) {
+  for (let x = 0; x < imgWidth; ++x) {
     const c = new color(x / (imgWidth - 1), y / (imgHeight - 1), 0.25)
     img.writeColor(c)
   }
